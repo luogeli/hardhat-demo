5f638aeb3a176c68b11e56107e809ad7c4c8b597
quhongwei <1160022906@qq.com>
2024-12-12 16:36:31 +0800

    fix: 修改fundMe合约需求注释

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,11 +4,13 @@ pragma solidity ^0.8.27;
 import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-// 需求：
-// 1. 创建一个收款函数
-// 2. 记录投资人并且查看
-// 3. 在锁定期内，到达目标值，生产商可以提款
-// 4. 在锁定期内，没有到达目标值，投资人在搜定期以后退款
+/**
+ * 需求：
+ * 1. 创建一个收款函数
+ * 2. 记录投资人并且查看
+ * 3. 在锁定期内，到达目标值，生产商可以提款
+ * 4. 在锁定期内，没有到达目标值，投资人在搜定期以后退款
+ */
 
 contract FundMe is ReentrancyGuard {
     // 投资人和投资金额mapping
